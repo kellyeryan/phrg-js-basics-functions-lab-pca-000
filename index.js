bbdd70c05df6f1813beb42d3fa3dcda016392086
kellyeryan <kellyeryan@gmail.com>
2019-12-31 11:40:00 -0500

    Finish js basics functions lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const hqBlock = 42
+
+function distanceFromHqInBlocks(value) {
+  return Math.abs(hqBlock - value)
+}
+
+function distanceFromHqInFeet(value) {
+  return distanceFromHqInBlocks(value) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start-destination) * 264
+}
+
+function calculatesFarePrice(start, destination)
+  {
+  if (distanceTravelledInFeet(start, destination) < 400) {
+    return 0
+  } else if
+    (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+    return (distanceTravelledInFeet(start, destination) -400) * .02
+  } else if
+    (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+    return 25
+  } else {
+    return "cannot travel that far";
+  }
+}
